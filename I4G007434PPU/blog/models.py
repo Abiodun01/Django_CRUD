d74fadc9929835fef5b75aad5ac0a8178b13bd74
Ajayi John <biodunajayijohn@gmail.com>
2022-06-24 23:28:38 +0100

    complete project

--- I4G007434PPU/blog/models.py
+++ I4G007434PPU/blog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.urls import reverse
 from django.template.defaultfilters import slugify
+
 # Create your models here.
 
 
--- I4G007434PPU/blog/views.py
+++ I4G007434PPU/blog/views.py
@@ -1,3 +1,33 @@
+from dataclasses import fields
+from pyexpat import model
+from django import views
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from .models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
+class PostListView(CreateView):
+    #specify model to be used
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+
+    model = Post
+
+class PostUpdateView(UpdateView):
+
+    model = Post
+    fields = "__alll__"
+    success_url = reverse_lazy("blog:all")
+    
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
